791a10dad0298cd61f18d50fb8d997939c49447a
Abhii27 <abhishekpanchal@1998>
2022-01-16 22:16:21 +0530

    Commited

---
+++ src/app/page-list/page-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-page-list',
+  templateUrl: './page-list.component.html',
+  styleUrls: ['./page-list.component.css']
+})
+export class PageListComponent implements OnInit {
+
+
+
+  @Input() pages: any[] = [];
+  xss = '<img src="" onerror="alert(123)" />';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
